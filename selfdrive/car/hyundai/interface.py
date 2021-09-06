e4c27b17b877c5f1d8af4fde8a77bfbea6c6affd
Circuit Pro Industries <oricalworks@gmail.com>
2021-09-06 11:31:49 -0400

    fixes

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -611,10 +611,10 @@ class CarInterface(CarInterfaceBase):
 
 
     if Params().get_bool('spasEnabled'):
-      if self.CS.mdps11_stat == 7 and not self.CC.lkas_active and not self.CC.spas_active and not self.CC.turning_indicator_alert:
-        events.add(EventName.buttonCancel)
-
-      if not self.CC.lkas_active and self.CS.mdps11_stat == 7 and self.CC.spas_active and not self.CC.turning_indicator_alert: # We need to alert driver when SPAS abort or fail.
+      if self.CS.mdps11_stat == 7 and not self.CC.turning_indicator_alert:
+        if not self.CC.lkas_active and not self.CC.spas_active:
+          events.add(EventName.buttonCancel)
+        elif not self.CC.lkas_active and self.CC.spas_active: # We need to alert driver when SPAS abort or fail.
         events.add(EventName.steerSaturated)
 
       if not self.CC.lkas_active and (self.CS.mdps11_stat == 6 or self.CS.mdps11_stat == 8):
