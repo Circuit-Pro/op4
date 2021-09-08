f7912a4842cc83a38e1a6015cd19ea32a6d39086
Circuit Pro Industries <oricalworks@gmail.com>
2021-09-08 18:18:37 -0400

    adjust engage ratelimit

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -139,7 +139,7 @@ class CarController():
         rate_limit = interp(CS.out.vEgo, ANGLE_DELTA_BP, ANGLE_DELTA_VU)
       apply_angle = clip(actuators.steeringAngleDeg, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit)
       if abs(apply_angle - CS.out.steeringAngleDeg) > 3:
-        rate_limit = 0.2
+        rate_limit = 0.4
         self.last_apply_angle = CS.out.steeringAngleDeg
         apply_angle = clip(actuators.steeringAngleDeg, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit)
 
