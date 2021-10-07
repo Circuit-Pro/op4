6b313aefbc524239c6f9606e7ce1b11bdc74695a
Circuit Pro Industries <oricalworks@gmail.com>
2021-10-07 03:58:04 -0400

    Update safety_hyundai_community.h

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -237,6 +237,18 @@ static int hyundai_community_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_f
             bus_fwd = 2;  // EON create EMS366 for MDPS
             OP_EMS_live -= 1;
           }
+          if (!OP_EMS_live || addr != 790) {
+            bus_fwd = fwd_to_bus1 == 1 ? 12 : 2;
+          } else {
+            bus_fwd = 2;  // EON create EMS366 for MDPS
+            OP_EMS_live -= 1;
+          }
+          if (!OP_EMS_live || addr != 881) {
+            bus_fwd = fwd_to_bus1 == 1 ? 12 : 2;
+          } else {
+            bus_fwd = 2;  // EON create EMS366 for MDPS
+            OP_EMS_live -= 1;
+          }
         } else {
           bus_fwd = fwd_to_bus1;  // EON create MDPS for LKAS
           OP_MDPS_live -= 1;
